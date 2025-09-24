199a6d8b899ed7db7e50527ae8367a8e7cdb8a9f
ASUDHARSON-2004-GIT <your-email@example.com>
2025-09-24 21:33:51 +0530

    Add counter Project

---
+++ counter/script.js
@@ -0,0 +1,24 @@
+const days = document.querySelector("#days");
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+
+
+
+function counter(){
+    const currentYear = new Date().getFullYear();
+    const newYear = new Date(`January 1 ${currentYear+1} 00:00:00`);
+
+    const currentDate = new Date();
+    const diff = newYear - currentDate;
+    const d = Math.floor(diff /1000/60/60/24);
+    const h = Math.floor((diff/1000/60/60)%24);
+    const m = Math.floor((diff/1000/60)%60);
+    const s = Math.floor((diff/1000)%60);
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML= s;
+}
+counter();
+setInterval(counter,1000);
